a3531cf507a74a49e088dca58c67b7b7e13d6a0e
mrtortuga <mrtortuga@ymail.com>
2017-08-02 10:13:01 -0400

    Added declarative implementation

--- src/main/java/com/teamtreehouse/jobs/App.java
+++ src/main/java/com/teamtreehouse/jobs/App.java
@@ -4,28 +4,57 @@ import com.teamtreehouse.jobs.model.Job;
 import com.teamtreehouse.jobs.service.JobService;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
 
-  public static void main(String[] args) {
-    JobService service = new JobService();
-    boolean shouldRefresh = false;
-    try {
-      if (shouldRefresh) {
-        service.refresh();
-      }
-      List<Job> jobs = service.loadJobs();
-      System.out.printf("Total jobs:  %d %n %n", jobs.size());
-      explore(jobs);
-    } catch (IOException e) {
-      e.printStackTrace();
+    public static void main(String[] args) {
+        JobService service = new JobService();
+        boolean shouldRefresh = false;
+        try {
+            if (shouldRefresh) {
+                service.refresh();
+            }
+            List<Job> jobs = service.loadJobs();
+            System.out.printf("Total jobs:  %d %n %n", jobs.size());
+            explore(jobs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private static void explore(List<Job> jobs) {
-      printPortlandJobsStream(jobs);
-  }
+    private static void explore(List<Job> jobs) {
+        getThreeJuniorJobsDeclaratively(jobs).forEach(System.out::println);
+    }
+
+    private static List<Job> getThreeJuniorJobsImperatively(List<Job> jobs) {
+        List<Job> juniorJobs = new ArrayList<>();
+        for (Job job : jobs) {
+            String title = job.getTitle().toLowerCase();
+            if (isJuniorJob(job)) {
+                juniorJobs.add(job);
+
+                if (juniorJobs.size() >= 3) {
+                    break;
+                }
+            }
+        }
+        return juniorJobs;
+    }
+
+    private static List<Job> getThreeJuniorJobsDeclaratively(List<Job> jobs) {
+        return jobs.stream()
+                .filter(App::isJuniorJob)
+                .limit(3)  // stops the filter early
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isJuniorJob(Job job) {
+        String title = job.getTitle().toLowerCase();
+        return title.contains("junior") || title.contains("jr");
+    }
 
     private static void printPortlandJobsStream(List<Job> jobs) {
     /*
@@ -42,9 +71,9 @@ public class App {
 
     private static void printPortlandJobsImperatively(List<Job> jobs) {
         for (Job job : jobs) {
-          if (job.getState().equals("OR") && job.getCity().equals("Portland")) {
-              System.out.println(job);
-          }
+            if (job.getState().equals("OR") && job.getCity().equals("Portland")) {
+                System.out.println(job);
+            }
         }
     }
 }
